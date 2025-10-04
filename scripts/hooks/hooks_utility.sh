c517912065c794172c4586cc7bbd34037bc95e24
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-04 11:06:29 -0700

    add new hooks_utility.sh with placeholder functions for logging and annotation marker search
    [^]hooks_utility.sh

--- scripts/hooks/hooks_utility.sh
+++ scripts/hooks/hooks_utility.sh
@@ -1,1 +1,40 @@
+################################################################################
+# hooks_utility.sh
+# TODO short descriptions
+#
+# author: kamiLeL
+# version: v1.0.0
+################################################################################
+
+
+# log message  #################################################################
+
 # TODO
+# print standard-formatted log message  ----------------------------------------
+_print_log_message() {
+
+}
+
+
+# hooks_utility_log_debug - print debug message  -------------------------------
+hooks_utility_log_debug() {}
+
+hooks_utility_log_info() {}
+
+hooks_utility_log_warning() {}
+
+hooks_utility_log_error() {}
+
+hooks_utility_log_critical() {}
+
+hooks_utility_log_critical() {}
+
+
+# search annotation markers  ###################################################
+
+# TODO
+_search_annotation_markers() {}
+
+hooks_utility_log_annotation_markers() {}
+
+hooks_utility_assert_annotation_markers() {}
