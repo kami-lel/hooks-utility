68211c78c62038d8a0dc702642d2c28022bd417f
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-10 14:15:45 -0500

    implement majority function of log style message

--- scripts/hooks/hooks_utility.sh
+++ scripts/hooks/hooks_utility.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ################################################################################
 # hooks_utility.sh
 # a collections of utility functions for git hooks
@@ -6,32 +8,97 @@
 # version: v1.0.0
 ################################################################################
 
-#  message  #################################################################
 
+# configurations
+ENABLE_ANSI_COLOR=1
+# messages, depending on their types, are sent to stdout & stderr respectively
+ENABLE_SPLIT_OUTPUT_STREAM=1
+
+
+# log style message  ###########################################################
+
+PREFIX_ERROR_DEBUG="DEBUG"
+PREFIX_ERROR_INFO="INFO "
+PREFIX_ERROR_WARNING="WARN "
+PREFIX_ERROR_ERROR="ERROR"
+PREFIX_ERROR_CRITICAL="CRIT "
+
+ANSI_COLOR_BLUE='\e[0;34m'
+ANSI_COLOR_YELLOW='\e[0;33m'
 ANSI_COLOR_RED='\e[0;31m'
 ANSI_RESET='\e[0m'
 
-# _hooks_utility_generate__message - create standardized log messages
+# _print_log_message - create standardized log messages  -----------------------
 # Arguments:
 #   message     message content (String)
-# other args: if date/time, if coloring, stdout/stderr
-_hooks_utility_generate__message() { 
-    # TODO
-    local message="$1"
-    echo -e "${ANSI_COLOR_RED}$message${ANSI_RESET}"
+#   level        integer
+_print_log_message() { 
+    local -i level="$2"
+    local ansi_reset="$ANSI_RESET"
+
+    # decide prefix & color based on level
+    local prefix ansi_color
+    case "$level" in
+    10)  # debug
+        prefix="$PREFIX_ERROR_DEBUG"
+        ansi_color="$ANSI_COLOR_BLUE"
+        ;;
+    20)  # info
+        prefix="$PREFIX_ERROR_INFO"
+        ansi_color="$ANSI_COLOR_YELLOW"
+        ;;
+    30)  # warning
+        prefix="$PREFIX_ERROR_WARNING"
+        ansi_color="$ANSI_COLOR_YELLOW"
+        ;;
+    40)  # error
+        prefix="$PREFIX_ERROR_ERROR"
+        ansi_color="$ANSI_COLOR_RED"
+        ;;
+    50)  # critical
+        prefix="$PREFIX_ERROR_CRITICAL"
+        ansi_color="$ANSI_COLOR_RED"
+        ;;
+    esac
+
+    # only use ansi function if set
+    if ! ((ENABLE_ANSI_COLOR)); then
+        ansi_color=""
+        ansi_reset=""
+    fi
+
+    # TODO date/time
+
+    # actually print
+    local content="${ansi_color}${prefix}${ansi_reset}:\t$1"
+    if ((ENABLE_SPLIT_OUTPUT_STREAM && level >= 40)); then
+        printf "%b\n" "$content" >&2  # print to stderr
+    else
+        printf "%b\n" "$content"  # print to stdout
+    fi
 }
 
 
-# hooks_utility__debug - print debug message  -------------------------------
-# hooks_utility__debug() {}
+# hooks_utility_debug - print debug message  -----------------------------------
+hooks_utility_debug() {
+    _print_log_message "$1" 10
+}
 
-# hooks_utility__info() {}
+hooks_utility_info() {
+    _print_log_message "$1" 20
+}
 
-# hooks_utility__warning() {}
+hooks_utility_warning() {
+    _print_log_message "$1" 30
+}
 
-# hooks_utility__error() {}
+hooks_utility_error() {
+    _print_log_message "$1" 40
+}
 
-# hooks_utility__critical() {}
+hooks_utility_critical() {
+    _print_log_message "$1" 50
+}
 
 
 
--- tests/log_message_test.sh
+++ tests/log_message_test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-
 # shellcheck source=/dev/null
 source "$(dirname "${BASH_SOURCE[0]}")/../scripts/hooks/hooks_utility.sh"
 
+echo \
+"tests for functions: Log Style Message  ########################################"
 
-# TODO
-_hooks_utility_generate_log_message "hi"
+echo \
+"log message of all types  ------------------------------------------------------"
+hooks_utility_debug "Some Debugging Message Content"
+hooks_utility_info "Content of Informational Message"
+hooks_utility_warning "Warning!"
+hooks_utility_error "Some Error Message"
+hooks_utility_critical "Giving Critical Error Message"
+
