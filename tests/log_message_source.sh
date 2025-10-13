6a5ea4f15bb5f09d18cf625f07a52a13c04ab607
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-13 17:17:47 -0500

    write some tests for source arg

--- tests/log_message_source.sh
+++ tests/log_message_source.sh
@@ -13,9 +13,23 @@ hooks_utility_warning "Warning!" "Information Test Component from this Script"
 hooks_utility_error "Some Error Message" "abc.sh::function.foo()?"
 
 echo \
-"combined w/ date time  -------------------------------------------------------\
+"combined w/ time  ------------------------------------------------------------\
 --"
 
 hooks_utility_debug -d "Some Debugging Message Content" "MainComponent"
-hooks_utility_warning -dt \
-        "Warning!" "Information Test Component from this Script"
+hooks_utility_debug -d "Some Debugging Message Content" "MainComponent"
+hooks_utility_info -d "Content of Informational Message" "MainComponent"
+hooks_utility_warning -d  "Warning!" "MainComponent"
+
+echo \
+"combined w/ date time  -------------------------------------------------------\
+--"
+
+hooks_utility_debug -dt "Some Debugging Message Content" \
+        "Information Test Component from Main Script"
+hooks_utility_debug -dt "Some Debugging Message Content" \
+        "Information Test Component from Main Script"
+hooks_utility_info -dt "Content of Informational Message" \
+        "Information Test Component from Main Script"
+hooks_utility_warning -dt "Warning!" \
+        "Information Test Component from Main Script"
