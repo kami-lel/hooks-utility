6601749309858611ebb03c86ed47e3a52910b8ac
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-13 18:01:54 -0500

    fix Test Script Utility Path: update source path in 2 log_message tests
    [~]log_message_date_time.sh: fix source path for hooks_utility.sh in log_message_date_time.sh test script
    [~]log_message_source.sh: fix script source path in log_message_source.sh to correct hooks_utility.sh location

--- tests/log_message/log_message_source.sh
+++ tests/log_message/log_message_source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck source=/dev/null
-source "$(dirname "${BASH_SOURCE[0]}")/../scripts/hooks/hooks_utility.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/../../scripts/hooks/hooks_utility.sh"
 
 echo \
 "tests: log style message, SOURCE argument  ###################################\
