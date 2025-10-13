9aec2fb380f9cb7af14fed890403999c878536ff
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-13 17:55:45 -0500

    implement function

--- scripts/hooks/hooks_utility.sh
+++ scripts/hooks/hooks_utility.sh
@@ -10,6 +10,9 @@
 
 
 # configurations
+# filtering log messages:
+# 10:debug & above, 20:information, 30:warning, 40:error, 50:critical
+LOGGING_LEVEL=10
 # use ANSI color codes when print to terminal
 ENABLE_ANSI_COLOR=1
 # messages, depending on their types, are sent to stdout & stderr respectively
@@ -34,9 +37,17 @@ DATE_FORMAT="%Y-%m-%d"
 TIME_FORMAT="%H:%M:%S"
 
 # create standardized log messages
-_print_log_message() { 
-    # parsing args & options
+_print_log_message() {
+    # filtering
     local -i level="$1"; shift
+
+    if [[ level -lt LOGGING_LEVEL ]]; then
+        # this message is filtered out
+        return 1
+    fi
+
+
+    # parsing rest args & options  ---------------------------------------------
 
     # parse options first
     local d_flag t_flag
@@ -128,7 +139,7 @@ _print_log_message() {
 # hooks_utility_debug()
 #
 # print a MESSAGE in log style message, prefixed with "DEBUG"
-# 
+#
 # USAGE:
 #   hooks_utility_debug [-d] [-t] MESSAGE [SOURCE]
 #
@@ -139,13 +150,14 @@ _print_log_message() {
 # OPTION:
 #   -d      contains current date in the printed message
 #   -t      contains current time in the printed message
-# 
+#
 # OUTPUT:
 #   print the given MESSAGE, in log style formatting, to stdout;
 #   utilizing ANSI coloring if stdout is a console
 #
 # RETURN:
 #   0       success
+#   1       not print because this message is filtered out by LOGGING_LEVEL
 #
 # EXAMPLE:
 #   hooks_utility_debug "some debug information"
@@ -159,7 +171,7 @@ hooks_utility_debug() {
 # hooks_utility_info()
 #
 # print a MESSAGE in log style message, prefixed with "INFO"
-# 
+#
 # USAGE:
 #   hooks_utility_info [-d] [-t] MESSAGE [SOURCE]
 #
@@ -173,7 +185,7 @@ hooks_utility_info() {
 # hooks_utility_warning()
 #
 # print a MESSAGE in log style message, prefixed with "WARN"
-# 
+#
 # USAGE:
 #   hooks_utility_warning [-d] [-t] MESSAGE [SOURCE]
 #
@@ -187,7 +199,7 @@ hooks_utility_warning() {
 # hooks_utility_error()
 #
 # print a MESSAGE in log style message, prefixed with "ERROR"
-# 
+#
 # USAGE:
 #   hooks_utility_error [-d] [-t] MESSAGE [SOURCE]
 #
@@ -206,7 +218,7 @@ hooks_utility_error() {
 # hooks_utility_critical()
 #
 # print a MESSAGE in log style message, prefixed with "CRIT"
-# 
+#
 # USAGE:
 #   hooks_utility_critical [-d] [-t] MESSAGE [SOURCE]
 #
