b12b286351929d66ca9895286ec64548ed5c4ffc
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-14 18:50:44 -0500

    update tests on combinations of args/options

--- tests/log_message/log_message_combine_test.sh
+++ tests/log_message/log_message_combine_test.sh
@@ -1,23 +1,45 @@
-# FIXME
+#!/bin/bash
+
+# shellcheck source=/dev/null
+source "$(dirname "${BASH_SOURCE[0]}")/../../scripts/hooks/hooks_utility.sh"
 
 echo \
-"combined w/ time  ------------------------------------------------------------\
---"
-
-hooks_utility_debug -d "Some Debugging Message Content" "MainComponent"
-hooks_utility_debug -d "Some Debugging Message Content" "MainComponent"
-hooks_utility_info -d "Content of Informational Message" "MainComponent"
-hooks_utility_warning -d  "Warning!" "MainComponent"
+"tests: log style message, combines args/optns  ###############################\
+##"
 
 echo \
-"combined w/ date time  -------------------------------------------------------\
+"source & date  ---------------------------------------------------------------\
 --"
 
-hooks_utility_debug -dt "Some Debugging Message Content" \
-        "Information Test Component from Main Script"
-hooks_utility_debug -dt "Some Debugging Message Content" \
-        "Information Test Component from Main Script"
-hooks_utility_info -dt "Content of Informational Message" \
-        "Information Test Component from Main Script"
-hooks_utility_warning -dt "Warning!" \
-        "Information Test Component from Main Script"
+echo "Some Debugging Message Content" | \
+        hooks_utility_debug -d "MainComponent"
+echo "Some Debugging Message Content" | \
+        hooks_utility_debug -d "MainComponent"
+echo "Content of Informational Message" | \
+        hooks_utility_info -d "MainComponent"
+echo "Warning!" | \
+        hooks_utility_warning -d  "MainComponent"
+
+echo \
+"source & date-time  ----------------------------------------------------------\
+--"
+
+echo "Some Debugging Message Content" | \
+        hooks_utility_debug -dt "Information Test Component from Main Script"
+echo "Some Debugging Message Content" | \
+        hooks_utility_debug -dt "Information Test Component from Main Script"
+echo "Content of Informational Message" | \
+        hooks_utility_info -dt "Information Test Component from Main Script"
+echo "Warning!" | \
+        hooks_utility_warning -dt "Information Test Component from Main Script"
+
+
+echo \
+"source & date & colors  ------------------------------------------------------\
+--"
+
+echo "Some Debugging Message Content" | \
+        hooks_utility_debug -dc "MainComponent"
+
+echo "Some Debugging Message Content" | \
+        hooks_utility_debug -dC "MainComponent"
