53850b5a712a01274ee98351c4c8fa0c8deaf2a8
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-10-14 18:28:54 -0500

    add comprehensive shell test for log message color variants, verify output to terminal and file with -c and -C options
    [/]log_message_color_test.sh

--- tests/log_message/log_message_color_test.sh
+++ tests/log_message/log_message_color_test.sh
@@ -1,1 +1,40 @@
-# TODO
+#!/bin/bash
+
+# shellcheck source=/dev/null
+source "$(dirname "${BASH_SOURCE[0]}")/../../scripts/hooks/hooks_utility.sh"
+
+tmp=$(mktemp)
+
+
+echo \
+"tests: log style message, -c/-C  #############################################\
+##"
+
+echo \
+"default behavior to terminal  ------------------------------------------------\
+--"
+
+echo "Some Error Message Content" | hooks_utility_error
+
+echo \
+"default behavior to file  ----------------------------------------------------\
+--"
+
+echo "Some Error Message Content" | hooks_utility_error 2> "${tmp}"
+cat "${tmp}"
+
+echo \
+"use -c  ----------------------------------------------------------------------\
+--"
+
+echo "Some Error Message Content" | hooks_utility_error -c
+echo "Some Error Message Content" | hooks_utility_error -c 2> "${tmp}"
+cat "${tmp}"
+
+echo \
+"use -C  ----------------------------------------------------------------------\
+--"
+
+echo "Some Error Message Content" | hooks_utility_error -C
+echo "Some Error Message Content" | hooks_utility_error -C 2> "${tmp}"
+cat "${tmp}"
