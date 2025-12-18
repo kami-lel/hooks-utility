4e1503f6d02818ac6dac7d3e30e520545ea000e9
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-18 15:36:10 -0800

    write tests for words

--- tests/colorful_print/colorful_print_various_words_test.sh
+++ tests/colorful_print/colorful_print_various_words_test.sh
@@ -3,10 +3,23 @@
 # shellcheck source=/dev/null
 source "$(dirname "${BASH_SOURCE[0]}")/../../hooks_utility.sh"
 
-# TODO write tests for words
-
 hooks_utility_colorful_print_pass
 printf '%b' "\n"
 
 hooks_utility_colorful_print_fail
+printf '%b' "\n"
+
+hooks_utility_colorful_print_debug
+printf '%b' "\n"
+
+hooks_utility_colorful_print_info
+printf '%b' "\n"
+
+hooks_utility_colorful_print_warning
+printf '%b' "\n"
+
+hooks_utility_colorful_print_error
+printf '%b' "\n"
+
+hooks_utility_colorful_print_critical
 printf '%b' "\n"
