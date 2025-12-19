1f52798d187b27e836388ff7482cf2de1ffa899a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-18 20:45:46 -0800

    finish tests

--- tests/colorful_print/colorful_print_smart_color_test.sh
+++ tests/colorful_print/colorful_print_smart_color_test.sh
@@ -6,15 +6,52 @@
 # shellcheck source=/dev/null
 source "$(dirname "${BASH_SOURCE[0]}")/../../hooks_utility.sh"
 
+tmp=$(mktemp)
+
 # FIXME use hooks_utility_colorful_print
 echo "colorful print smart color test" | hooks_utility_padding_centered '#'
+
+# default to terminal  =========================================================
+echo "default to terminal" | hooks_utility_padding_centered '='
+
 echo "Some Red Content" | _colorful_print "${ANSI_COLOR_RED}" 1
 printf '\n'
+
 echo "Some Green Content" | _colorful_print "${ANSI_COLOR_GREEN}" 1
 printf '\n'
 
-echo "default to terminal" | hooks_utility_padding_centered '='
-
+# default to file  =============================================================
 echo "default to file" | hooks_utility_padding_centered '='
+
+echo "Some Yellow Content" |
+    _colorful_print "${ANSI_COLOR_YELLOW}" 1 2>"${tmp}"
+cat "${tmp}"
+printf '\n'
+
+echo "Some Blue Content" | _colorful_print "${ANSI_COLOR_BLUE}" 1 2>"${tmp}"
+cat "${tmp}"
+printf '\n'
+
+# use -c  ======================================================================
 echo "use -c" | hooks_utility_padding_centered '='
+
+echo "Some Purple Content to Terminal" |
+    _colorful_print "${ANSI_COLOR_PURPLE}" 1
+printf '\n'
+
+echo "Some Cyan Content to File" |
+    _colorful_print "${ANSI_COLOR_CYAN}" 1 2>"${tmp}"
+cat "${tmp}"
+printf '\n'
+
+# use -C  ======================================================================
 echo "use -C" | hooks_utility_padding_centered '='
+
+echo "Some Red Content to Terminal" |
+    _colorful_print "${ANSI_COLOR_RED}" 1
+printf '\n'
+
+echo "Some Green Content to File" |
+    _colorful_print "${ANSI_COLOR_GREEN}" 1 2>"${tmp}"
+cat "${tmp}"
+printf '\n'
--- tests/log_message/log_message_color_test.sh
+++ tests/log_message/log_message_color_test.sh
@@ -7,19 +7,27 @@ tmp=$(mktemp)
 
 echo "log style message test on -c/-C" | hooks_utility_padding_centered '#'
 
+# default to terminal  =========================================================
 echo "default to terminal" | hooks_utility_padding_centered '='
 echo "Some Error Message Content" | hooks_utility_error
 
+# default to file  =============================================================
 echo "default to file" | hooks_utility_padding_centered '='
 echo "Some Error Message Content" | hooks_utility_error 2>"${tmp}"
 cat "${tmp}"
 
+# use -c  ======================================================================
 echo "use -c" | hooks_utility_padding_centered '='
-echo "Some Error Message Content" | hooks_utility_error -c
-echo "Some Error Message Content" | hooks_utility_error -c 2>"${tmp}"
+
+echo "Some Error Message Content to Terminal" | hooks_utility_error -c
+
+echo "Some Error Message Content to File" | hooks_utility_error -c 2>"${tmp}"
 cat "${tmp}"
 
+# use -C  ======================================================================
 echo "use -C" | hooks_utility_padding_centered '='
-echo "Some Error Message Content" | hooks_utility_error -C
-echo "Some Error Message Content" | hooks_utility_error -C 2>"${tmp}"
+
+echo "Some Error Message Content to Terminal" | hooks_utility_error -C
+
+echo "Some Error Message Content to File" | hooks_utility_error -C 2>"${tmp}"
 cat "${tmp}"
