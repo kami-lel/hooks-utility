a91bdb7b4acfa28f7b7b5dfe818a32e68e21bbe1
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-19 02:54:19 -0800

    update test format

--- tests/padding_print/padding_print_basic_test.sh
+++ tests/padding_print/padding_print_basic_test.sh
@@ -5,12 +5,15 @@ source "$(dirname "${BASH_SOURCE[0]}")/../../hooks_utility.sh"
 
 echo "left just w/ #:"
 echo "Title" | hooks_utility_padding_left_just "#"
+printf '\n'
 
 echo "right just w/ =:"
 echo "Some Information" | hooks_utility_padding_right_just "="
+printf '\n'
 
 echo "centered w/ *:"
 echo "Centered Heading" | hooks_utility_padding_centered "*"
+printf '\n'
 
 echo "centered w/ odd width:"
 echo "Centering Heading" | hooks_utility_padding_centered "-"
