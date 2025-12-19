0d162c2e22ccde27c45cdeda5e4bd0698e5f37d1
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-18 17:47:32 -0800

    slightly modify bp

--- hooks_utility.sh
+++ hooks_utility.sh
@@ -550,7 +550,7 @@ _parse_adding_padding() {
 #   0   success: pass or skip checks
 #   1   failure: undesired AM detected
 hooks_utility_protect_branch() {
-    echo "" | hooks_utility_enter "${BP_DISPLAY_NAME}"
+    echo 'branch protection' | hooks_utility_enter "${BP_DISPLAY_NAME}"
 
     local commit_type
     commit_type=$(get_commit_type_at_pre_commit)
@@ -580,7 +580,7 @@ $(_search_am_from_git_diff_cached 2)"
             hooks_utility_fail "${BP_DISPLAY_NAME}"
         return 1
     else
-        echo "" |
+        echo 'branch protection' |
             hooks_utility_pass "${BP_DISPLAY_NAME}"
         return 0
     fi
