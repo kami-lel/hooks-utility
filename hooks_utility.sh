eec11ac7007710c3136e896896b3fed4b2843bb0
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-22 05:14:12 -0800

    update version check logic

--- hooks_utility.sh
+++ hooks_utility.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # a collections of utility functions for git hooks
 #
 # author:  kamiLeL
-# version: v2.0.1-alpha
+# version: v2.0.2
 ################################################################################
 
 # configurations  ##############################################################
