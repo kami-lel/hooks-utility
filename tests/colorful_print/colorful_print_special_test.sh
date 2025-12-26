73dc88f1806d37a2e9f74cd35509e8f0a37c0a6a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-26 14:56:20 -0800

    update structure for tests

--- tests/colorful_print/colorful_print_special_test.sh
+++ tests/colorful_print/colorful_print_special_test.sh
@@ -6,3 +6,15 @@
 source "$(dirname "${BASH_SOURCE[0]}")/../../hooks_utility.sh"
 
 # TODO
+
+echo "special character tests" | hooks_utility_padding_centered '#'
+
+# as literal  ==================================================================
+echo "as literal" | hooks_utility_padding_centered '='
+printf '%s' "\n" | hooks_utility_padding_centered '-'
+printf '%s' "\t" | hooks_utility_padding_centered '-'
+
+# as interpreted  ==============================================================
+echo "as interpreted" | hooks_utility_padding_centered '='
+printf '%s' "\n" | hooks_utility_padding_centered '-'
+printf '%s' "\t" | hooks_utility_padding_centered '-'
