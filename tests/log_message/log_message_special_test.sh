14837475cb390408f477e219df85ee972b69687f
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-26 15:35:42 -0800

    update bug info

--- tests/log_message/log_message_special_test.sh
+++ tests/log_message/log_message_special_test.sh
@@ -5,30 +5,25 @@
 # shellcheck source=/dev/null
 source "$(dirname "${BASH_SOURCE[0]}")/../../hooks_utility.sh"
 
-# TODO
-
 echo "special character tests" | hooks_utility_padding_centered '#'
 # as literal  ==================================================================
+# BUG
 echo "as literal" | hooks_utility_padding_centered '='
 
 # \n  --------------------------------------------------------------------------
 printf '%s' "\n" | hooks_utility_padding_centered '-'
-
-printf '\n'
+printf 'AAAAA%sZZZZZ' "\n" | hooks_utility_info
 
 # \t  --------------------------------------------------------------------------
 printf '%s' "\t" | hooks_utility_padding_centered '-'
-
-printf '\n'
+printf 'AAAAA%sZZZZZ' "\t" | hooks_utility_error
 
 # as interpreted  ==============================================================
 echo "as interpreted" | hooks_utility_padding_centered '='
 # \n  --------------------------------------------------------------------------
 printf '%s' "\n" | hooks_utility_padding_centered '-'
-
-printf '\n'
+printf 'AAAAA%bZZZZZ' "\n" | hooks_utility_info
 
 # \t  --------------------------------------------------------------------------
 printf '%s' "\t" | hooks_utility_padding_centered '-'
-
-printf '\n'
+printf 'AAAAA%bZZZZZ' "\t" | hooks_utility_error
