03b3e7cbb0be55cb43eadc9b728a0c103e6f2233
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-26 14:59:45 -0800

    update tests for special chars

--- tests/colorful_print/colorful_print_special_test.sh
+++ tests/colorful_print/colorful_print_special_test.sh
@@ -11,10 +11,25 @@ echo "special character tests" | hooks_utility_padding_centered '#'
 
 # as literal  ==================================================================
 echo "as literal" | hooks_utility_padding_centered '='
+
+# \n  --------------------------------------------------------------------------
 printf '%s' "\n" | hooks_utility_padding_centered '-'
+printf 'AAAAA%sZZZZZ' "\n" | hooks_utility_colorful_print "\e[0;31m"
+printf '\n'
+
+# \t  --------------------------------------------------------------------------
 printf '%s' "\t" | hooks_utility_padding_centered '-'
+printf 'AAAAA%sZZZZZ' "\t" | hooks_utility_colorful_print "\e[0;32m"
+printf '\n'
 
 # as interpreted  ==============================================================
 echo "as interpreted" | hooks_utility_padding_centered '='
+# \n  --------------------------------------------------------------------------
 printf '%s' "\n" | hooks_utility_padding_centered '-'
+printf 'AAAAA%bZZZZZ' "\n" | hooks_utility_colorful_print "\e[0;31m"
+printf '\n'
+
+# \t  --------------------------------------------------------------------------
 printf '%s' "\t" | hooks_utility_padding_centered '-'
+printf 'AAAAA%bZZZZZ' "\t" | hooks_utility_colorful_print "\e[0;32m"
+printf '\n'
