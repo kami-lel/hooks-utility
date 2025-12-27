90da539b68fdfbf73ec79fa09352e1e7a78fe094
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2025-12-26 16:15:24 -0800

    bump alpha test version

--- hooks_utility.sh
+++ hooks_utility.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # a collections of utility functions for git hooks
 #
 # author:  kamiLeL
-# version: v2.2.0
+# version: v2.2.1-alpha
 ################################################################################
 
 # configurations  ##############################################################
